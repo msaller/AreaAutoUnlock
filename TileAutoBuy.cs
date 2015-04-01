ef4e837cfa3b53791abb12d4faedff56c4435a44
msaller <msaller@kamundo.de>
2015-04-01 15:11:12 +0200

    Initial commit
    Initial

---
+++ TileAutoBuy.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TileAutoBuy
+{
+    using ICities;
+    public class TileAutoBuy : IUserMod
+    {
+        public string Name
+        {
+            get { return "TileAutoBuy"; }
+        }
+
+        public string Description
+        {
+            get { return "Automatically buy all tiles on game start"; }
+        }
+    }
+}
---
+++ TileAutoBuyLoader.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using ColossalFramework.Plugins;
+using ICities;
+using UnityEngine;
+
+namespace TileAutoBuy
+{
+    public class TileAutoBuyLoader : ILoadingExtension
+    {
+
+        private IManagers managers;
+
+        public void OnLevelLoaded(LoadMode mode)
+        {
+            DebugOutputPanel.AddMessage(PluginManager.MessageType.Message, "TileAutoBuy goes");
+            if (mode != LoadMode.NewGame)
+                return;
+            DebugOutputPanel.AddMessage(PluginManager.MessageType.Message, "TileAutoBuy goes +mode");
+
+            // buy tiles to 0, 0
+            this.managers.areas.UnlockArea(1, 2, false);
+            this.managers.areas.UnlockArea(0, 2, false);
+            this.managers.areas.UnlockArea(0, 1, false);
+            this.managers.areas.UnlockArea(0, 0, false);
+
+            // buy the rest
+            for (int x = 0; x <= 4; x++)
+            {
+                for (int y = 0; y <= 4; y++)
+                {
+                    if (!this.managers.areas.IsAreaUnlocked(x, y))
+                    {
+                        this.managers.areas.UnlockArea(x, y, false);
+                    }
+                }
+            }
+        }
+
+        public void OnCreated(ILoading loading)
+        {
+            this.managers = loading.managers;
+        }
+
+        public void OnLevelUnloading()
+        {
+        }
+
+        public void OnReleased()
+        {
+        }
+    }
+}
