b6d09320ad6be1e41a6cca5f7fccdf7df65acca7
msaller <msaller@kamundo.de>
2015-04-02 03:58:37 +0200

    Changed name, added 25 tile unlock possibility

---
+++ AreaAutoUnlock.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using ICities;
+using UnityEngine;
+using ColossalFramework.Plugins;
+
+namespace AreaAutoUnlock
+{
+
+
+    public class AreaAutoUnlock : IUserMod
+    {
+        public void OnLoad()
+        {
+
+        }
+
+        public void OnUnload()
+        {
+        }
+
+        public string Name
+        {
+            get { return "TileAutoBuy"; }
+        }
+
+        public string Description
+        {
+            get { return "Automatically unlock all 25 areas"; }
+        }
+    }
+
+    public class AreaAutoUnlockAreasExtension : AreasExtensionBase
+    {
+        public override void OnCreated(IAreas areas)
+        {
+            areas.maxAreaCount = 25;
+        }
+    }
+
+    public class AreaAutoUnlockLoadingExtension : LoadingExtensionBase
+    {
+
+        public override void OnLevelLoaded(LoadMode mode)
+        {
+            if (mode != LoadMode.NewGame)
+                return;
+
+            // buy tiles to 0, 0
+            this.managers.areas.UnlockArea(1, 2, false);
+            this.managers.areas.UnlockArea(0, 2, false);
+            this.managers.areas.UnlockArea(0, 1, false);
+            this.managers.areas.UnlockArea(0, 0, false);
+
+            // buy the rest
+            for (int x = 0; x <= 4; x++)
+            {
+                for (int y = 0; y <= 4; y++)
+                {
+                    if (!this.managers.areas.IsAreaUnlocked(x, y))
+                    {
+                        this.managers.areas.UnlockArea(x, y, false);
+                    }
+                }
+            }
+        }
+    }
+}
--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TileAutoBuy")]
+[assembly: AssemblyTitle("AreaAutoUnlock")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TileAutoBuy")]
+[assembly: AssemblyProduct("AreaAutoUnlock")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("acdccd09-0110-4a07-8946-939eedccec3e")]
+[assembly: Guid("93e44ac1-5041-446f-bce9-6614858213cf")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.*")]
-//[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
